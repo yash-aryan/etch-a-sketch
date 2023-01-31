40901bc9b04195c6a5bd644dea883f90a7a8bac6
Yash <79582883+yash-aryan@users.noreply.github.com>
2023-01-31 18:26:10 +0530

    Update grid flex properties & Add clear button
    The grid now always takes up fixed amount of space on screen, and each
    grid-cells that gets added also get smaller to fit-in.
    Also added logic to clear grid, or clear all drawing on a press of a
    button.
    Also updated index.html & stlyes.css to improve the webpage's design.

--- scripts.js
+++ scripts.js
@@ -1,43 +1,69 @@
+"use strict";
+
+// --- DOM references ---
 const container = document.querySelector('.grid-container');
 const body = document.querySelector('body');
-createGrid();
+const rowCount = document.querySelector('.range-slider');
+const selectedGridSize = document.querySelectorAll('.slider-value');
+const clearGridButton = document.querySelector('.clear-grid');
 
-// Gets the nodelist of all grid-cells and converts it into an array which is stored in gridCells variable
-let gridCells = Array.from(container.getElementsByClassName('grid-cell'));
+// --- Main Code ---
+// calls "createGrid()" with the selected value from range input as the argument
+selectedGridSize.forEach((n) => {
+  n.innerHTML = rowCount.value;
+  createGrid(rowCount.value);
+  rowCount.addEventListener('input', () => {
+    n.innerHTML = rowCount.value;
+    createGrid(rowCount.value);
+  });
+});
 
 container.addEventListener('mousedown', startDrawing);
 body.addEventListener('mouseup', stopDrawing);
 
-// Function Declarations Below
+clearGridButton.addEventListener('click', clearGrid);
 
-// Creates a grid that utlises flexbox technique, and adds relevant class to each grid-row and grid-cell
-// By default, it creates a 16x16 grid
-function createGrid(inputRows = 16) {
-    let i = 1;
-    while (i <= inputRows) {
-        const row = document.createElement('div');
-        row.classList.add('grid-row');
-        container.appendChild(row);
-        let j = 1;
-        while (j <= inputRows) {
-            const cell = document.createElement('div');
-            cell.classList.add('grid-cell');
-            row.appendChild(cell);
-            j++;
-        }
-        i++;
+// --- Function Declarations ---
+// Creates a grid using flexbox method, and adds relevant classes to each grid-row and grid-cell
+// By default, it creates a 16x16 grid, because range input value by default is set to 16
+function createGrid(inputRows) {
+  container.innerHTML = '';
+  let i = 1;
+  while (i <= inputRows) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+    container.appendChild(row);
+    let j = 1;
+    while (j <= inputRows) {
+      const cell = document.createElement('div');
+      cell.classList.add('grid-cell');
+      row.appendChild(cell);
+      j++;
     }
+    i++;
+  }
 }
 
 function startDrawing() {
-    for (let n of gridCells) n.addEventListener('mousemove', changeCellColor);
+  for (let n of container.getElementsByClassName('grid-cell')) {
+    n.addEventListener('mousemove', changeCellColor);
+  }
 }
 
 function stopDrawing() {
-    for (let n of gridCells) n.removeEventListener('mousemove', changeCellColor);
+  for (let n of container.getElementsByClassName('grid-cell')) {
+    n.removeEventListener('mousemove', changeCellColor);
+  }
 }
 
-// .active class is added to each grid-cell element in DOM, check the style properties of .active class in styles.css
+// When called, adds ".active" class to the drawn grid-cell elements in DOM
 function changeCellColor(e) {
-    e.target.classList.add('active');
+  e.target.classList.add('active');
+}
+
+// When called, removes ".active" class from all grid-cells elements in DOM
+function clearGrid() {
+  for (let n of container.getElementsByClassName('grid-cell')) {
+    n.classList.remove('active');
+  }
 }
